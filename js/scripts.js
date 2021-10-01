95363b014e26fbf9530b435a0dd1cb1be9a38f6c
Kris Hokr <khokr@ucdavis.edu>
2021-10-01 11:52:46 -0700

    edited scripts.js to display pokemon on page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 let pokemonList = [
-    {name: 'Bulbasaur', age: 7, types: ['poison', 'grass']},
-    {name: 'Charmander', age: 3, types: ['fire']}, 
-    {name: 'Squirtle', age: 2, types: ['water']}
+    {name: 'Bulbasaur', height: 0.7, types: ['poison', 'grass']},
+    {name: 'Charizard', height: 1.7, types: ['fire', 'flying']}, 
+    {name: 'Squirtle', height: 0.5, types: ['water']}
 ];
+
+for (let i=0; i<pokemonList.length; i++) {
+
+    let pokemonName = pokemonList[i].name;
+    let pokemonHeight = pokemonList[i].height;
+
+    let note;
+    if (pokemonHeight > 1) {
+        note = 'So big!!'
+    }
+    
+    let result = `<br>${pokemonName} (Height: ${pokemonHeight}) ${note || ''}<br>` ;
+    
+    document.write(result);
+}
+
 
