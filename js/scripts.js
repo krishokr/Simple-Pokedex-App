a7616319c021a9a7edb1eb03af0bd3b47070d7b5
Kris Hokr <khokr@ucdavis.edu>
2021-09-30 18:19:11 -0700

    changed title of index.html, added pokemonList array that contains different pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert("Hello world");
+let pokemonList = [
+    {name: 'Bulbasaur', age: 7, types: ['poison', 'grass']},
+    {name: 'Charmander', age: 3, types: ['fire']}, 
+    {name: 'Squirtle', age: 2, types: ['water']}
+];
 
-let favoriteFood = "pizza";
-document.write(favoriteFood);
