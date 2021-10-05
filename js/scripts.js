94ec2a6e52505d8eca09e48f40e280a4f1a2100b
Kris Hokr <khokr@ucdavis.edu>
2021-10-05 10:56:51 -0700

    removed unnecessary functions, added an IIFE for displaying pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ let pokemonRepository = (function() {
     function getAll() { return pokemonList};
     
     function add(pokemon) {
-        //This might be too long...
+
         return itemIsObjectWithSpecifiedProperties(pokemon) ? pokemonList.push(pokemon) : console.log("Cannot add a non-object or an object without specified properties: name, height, types");
     };
     
@@ -34,43 +34,29 @@ let pokemonRepository = (function() {
 // Adding pokemon to the pokemon repository
 pokemonRepository.add({name: 'Bulbasaur', height: 0.7, types: ['poison', 'grass']});
 pokemonRepository.add({name: 'Charizard', height: 1.7, types: ['fire', 'flying']});
-pokemonRepository.add({name: 'Squirtle', height: 0.5, types: ['water']})
+pokemonRepository.add({name: 'Squirtle', height: 0.5, types: ['water']});
 
-// Display Pokemon Helper Functions
-let pokemonList = () => pokemonRepository.getAll();
-
-let getName = pokemon => pokemon.name;
-
-let getHeight = pokemon => pokemon.height;
-
-function getNote(height) {
-    let note = '';
-    if (height > 1) {
-        note = 'So big!!'
-    }
-    return note;
-};
-
-function pokemonDescription(name, height, note) {
-    return `<br>${name} (Height: ${height}) ${note}<br>` 
-};
-
-//Unsure if I should have this function, or if I should just include it in the .forEach()
-function writePokemon(pokemon) {
-    let name = getName(pokemon);
-    let height = getHeight(pokemon);
-    let note = getNote(height);
-    let description = pokemonDescription(name, height, note);
-        
-    return document.write(description);
-}
-
-function displayPokemonArray (pokemonList) {
-    return pokemonList.forEach(pokemon => writePokemon(pokemon));
-}
 
 //Displaying Pokemon on Site
-displayPokemonArray(pokemonList());
+(function() {
+    let pokemonList = pokemonRepository.getAll();
+
+    function getNote(height) {
+        let note = '';
+        if (height > 1) {
+            note = 'So big!!'
+        }
+        return note;
+    };
+
+    return pokemonList.forEach(function(pokemon) {
+            let name = pokemon.name;
+            let height = pokemon.height;
+            let note = getNote(height);
+            
+            return document.write(`<br>${name} (Height: ${height}) ${note}<br>`);
+        });
+})();
 
 
 
