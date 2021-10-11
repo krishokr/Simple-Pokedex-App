e645c03e567615a4c3d9761e14e061f1992971e2
Kris Hokr <khokr@ucdavis.edu>
2021-10-11 16:36:15 -0700

    removed second IIFE and added those functions to first IIFE, rearranged comments

--- js/scriptsv2.js
+++ js/scriptsv2.js
@@ -2,61 +2,34 @@ let pokemonRepository = (function() {
     let pokemonList = []; //this is an array of normal objects
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-    //Functions for loading API
+    //Functions for loading initial API to get pokemon object with name and detailsUrl properties
     function loadList() {
         return fetch(apiUrl).then(function(response) {return response.json()}).then(function(json) {
-            
-            json.results.forEach(function(item) { //results is a property in the json object that holds the name and url
+            //results is a property in the json object that holds the name and url
+            json.results.forEach(function(item) { 
                 let pokemon = { //turns json object into real object
                     name: item.name,
                     detailsUrl: item.url
                 }
-
-                add(pokemon); //pokemon here is a real object
-                
-            }); //this is returning a promise, but of undefined since there is no return value. can be used later
+                add(pokemon); //pokemon here is a real object               
+            }); //successful fetch returns undefined promise since there's no return value
            
         }).catch (e => console.error(e))
     }
-        //this function below must be called inside of a pokemonRepository.getAll().forEach() loop
-    function loadDetails(item) { //loading details for each pokemon in pokemonList array --> 
-        let url = item.detailsUrl; // there's an item parameter since need to get the details url from each pokemon
+    
+    //Gets pokemon api details via the detailsUrl property from each pokemon object (found initially from loadList())
+    function loadDetails(item) { 
+        let url = item.detailsUrl; //there's an item parameter since need to get the details url from each pokemon
         return fetch(url).then(response=> response.json()).then(function(json) {
-            item.imageUrl = json.sprites.front_default; //adding new properties imageUrl, height, types to each item                                                   
+            item.imageUrl = json.sprites.front_default;                                                
             item.height = json.height; 
             item.types = json.types;
         }).catch (e => console.error(e)) //RETURNS A PROMISE!!
     }
 
-
-    //Helper functions for item inspection
-    function checkItemType(item) {
-        //checks to see if item is an object
-        return (typeof item === 'object') ? item : false;
-    }
-
-    //Helper Functions for Methods
-    function getAll() { return pokemonList};
-    
-    function add(pokemon) {
-        return checkItemType(pokemon) ? (pokemonList.push(pokemon)) : console.log("Cannot add a non-object or an object without specified properties: name, height, types");
-    };
-    
-    return {
-        getAll: getAll,
-        add: add,
-        loadList: loadList,
-        loadDetails: loadDetails
-    }
-})();
-
-
-//Displaying Pokemon on Site
-(function() {
-
-
+    //Displays pokemon object in the console using loadDetails() function above
     function showDetails(pokemon) { 
-        pokemonRepository.loadDetails(pokemon).then(function() {
+        loadDetails(pokemon).then(function() {
             console.log(pokemon);
         }).catch(e => console.error(e))
     }
@@ -79,18 +52,36 @@ let pokemonRepository = (function() {
         pokemonButton.addEventListener('click', function() {return showDetails(pokemon)});
     }
 
-    //loadList() loads initial api and fills pokemonList in repository with pokemon objects including name and details url
-    //loadDetails() loads details url, and then fills the pokemon objects with height, imageUrl, and types
-    //showDetails() calls loadDetails() and displays the properties in the console
+    //Helper functions for item inspection
+    function checkItemType(item) {
+        //checks to see if item is an object
+        return (typeof item === 'object') ? item : false;
+    }
 
-    pokemonRepository.loadList().then(function() { //no parameter here since .loadList() doesn't have a return value
-        //console.log(pokemonRepository.getAll()) if want to print entire list, need to do this after calling loadList()
-        pokemonRepository.getAll().forEach(function(pokemon) {
-            addListItem(pokemon);           
-        })
-    })   
+    //Helper Functions for Methods
+    function getAll() { return pokemonList};
+    
+    function add(pokemon) {
+        return checkItemType(pokemon) ? (pokemonList.push(pokemon)) : console.log("Cannot add a non-object or an object without specified properties: name, height, types");
+    };
+    
+    return {
+        getAll: getAll,
+        add: add,
+        loadList: loadList,
+        addListItem: addListItem
+    }
 })();
 
 
+//Displaying Pokemon on Site
+pokemonRepository.loadList().then(function() { //no parameter here since .loadList() doesn't have a return value
+    pokemonRepository.getAll().forEach(function(pokemon) {
+        pokemonRepository.addListItem(pokemon);           
+    })
+})   
 
-//Qs: why can I not just print pokemonRepository.getAll() without first calling .loadList()?
+//Notes:
+    //loadList() loads initial api and fills pokemonList in repository with pokemon objects including name and details url
+    //loadDetails() loads details url, and then fills the pokemon objects with height, imageUrl, and types
+    //showDetails() calls loadDetails() and displays the properties in the console
